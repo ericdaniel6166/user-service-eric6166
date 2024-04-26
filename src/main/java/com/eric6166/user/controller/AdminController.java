ff9304768a5c5da2414318d768f44177362ec6fe
eric6166 <ericnguyen6166@gmail.com>
2024-04-26 15:45:38 +0700

    commit

---
+++ src/main/java/com/eric6166/user/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.eric6166.user.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@Validated
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AdminController {
+
+    @Operation(security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("admin test");
+    }
+
+}
