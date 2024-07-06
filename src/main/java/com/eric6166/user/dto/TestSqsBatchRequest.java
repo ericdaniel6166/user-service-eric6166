1dda75b0554c6f18a899ba4cdcf3882dbee91041
eric6166 <ericnguyen6166@gmail.com>
2024-07-06 11:17:18 +0700

    commit

--- src/main/java/com/eric6166/user/dto/TestSqsBatchRequest.java
+++ src/main/java/com/eric6166/user/dto/TestSqsBatchRequest.java
@@ -1,26 +1,33 @@
 package com.eric6166.user.dto;
 
-import com.eric6166.aws.sqs.SqsSendMessageBatchRequestEntry;
 import com.eric6166.aws.sqs.SqsSendMessageBatchRequestEntries;
+import com.eric6166.aws.sqs.SqsSendMessageBatchRequestEntry;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TestSqsBatchRequest implements SqsSendMessageBatchRequestEntries {
 
-    String queueName;
+    @NotBlank
+    @NotEmpty
     Collection<Message> messages;
+    @NotNull
     Integer delaySeconds;
+    @NotBlank
+    String queueName;
+    @NotBlank
     String messageGroupId;
 
     @Override
     public Collection<SqsSendMessageBatchRequestEntry> getSqsSendMessageBatchRequestEntries() {
-        return messages.stream().map(o -> (SqsSendMessageBatchRequestEntry) o).collect(Collectors.toList());
+        return messages.stream().map(o -> (SqsSendMessageBatchRequestEntry) o).toList();
     }
 
     @Data
